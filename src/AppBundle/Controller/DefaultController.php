7e0086c568c723f5e6d5d051c2885f432f397f61
flottin <flotitn@gmail.com>
2020-02-22 12:42:05 +0100

    add siret management

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -29,8 +29,78 @@ MultiProcessService $multiProcessService)
     }
 
 
+    //Fonction algorithme de Luhn
+    public function isLuhnNum($num)
+    {
+        //longueur de la chaine $num
+        $length = strlen($num);
+
+        //resultat de l'addition de tous les chiffres
+        $tot = 0;
+        for($i=$length-1;$i>=0;$i--)
+        {
+            $digit = substr($num, $i, 1);
+
+            if ((($length - $i) % 2) == 0)
+            {
+                $digit = $digit*2;
+                if ($digit>9)
+                {
+                    $digit = $digit-9;
+                }
+            }
+            $tot += $digit;
+        }
+
+        return (($tot % 10) == 0);
+    }
 
 
+    /**
+ * @Route("/entreprise/verification")
+ */
+    public function verificationAction(Request $request)
+    {
+
+        $siret = $request->get('siret');
+        $siretExemple = '34368801600504';
+
+        $isValid = $this->isLuhnNum($siret);
+
+        $isValidString = $isValid ? 'true' : 'false';
+
+
+        // replace this example code with whatever you need
+        return $this->render('entreprise/index.html.twig',
+            ['isValidSiret' => $isValidString,
+                'siret' => $siret,
+                'siretExemple' => $siretExemple,
+                ]);
+    }
+
+    /**
+     * @Route("/entreprise/verification/modal")
+     */
+    public function verificationModalAction(Request $request)
+    {
+
+        // replace this example code with whatever you need
+        return $this->render('entreprise/modal.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/api")
+     */
+    public function apiAction(Request $request)
+    {
+return $this->json([]);
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
 
     /**
      * @Route("/", name="homepage")
